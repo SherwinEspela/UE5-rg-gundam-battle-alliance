be3fee5d5e195bb4bc47c898011983194a35ded0
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2024-07-31 14:47:38 -0700

    Adding Character class and blueprint

---
+++ RGGundamBA/Source/RGGundamBA/Private/Characters/GundamRX93Character.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/GundamRX93Character.h"
+
+AGundamRX93Character::AGundamRX93Character()
+{
+
+}
---
+++ RGGundamBA/Source/RGGundamBA/Public/Characters/GundamRX93Character.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/RGGBaseCharacter.h"
+#include "GundamRX93Character.generated.h"
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class RGGUNDAMBA_API AGundamRX93Character : public ARGGBaseCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	AGundamRX93Character();
+
+};
